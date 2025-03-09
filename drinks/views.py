8dc219a09183a2561af920e3ca56f6f15d5283e8
Maunducyrus <maunducyrus123@gmail.com>
2025-03-10 01:53:10 +0300

    views

--- drinks/views.py
+++ drinks/views.py
@@ -1,19 +1,5 @@
 # Create your API endpoints here
 
-# from django.http import HttpResponse, JsonResponse
-# from .models import Drink
-# from .serializers import DrinkSerializer
-
-# def drink_list(request):
-#     return HttpResponse("List of drinks")
-
-# def get_drinks(request):
-#     # get all the drinks
-#     drinks = Drink.objects.all()
-#     # serialize them
-#     serializer = DrinkSerializer(drinks, many=True)
-#     # return them as a JSON response
-#     return JsonResponse(serializer.data, safe=False)
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
