2515d0423430318d19f5d4d4d5a5406026f9d57e
Maunducyrus <maunducyrus123@gmail.com>
2025-03-10 00:49:01 +0300

    class Meta

--- drinks/serializers.py
+++ drinks/serializers.py
@@ -3,4 +3,7 @@ from rest_framework import serializers
 from .models import Drink
 
 class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = '__all__'
     
