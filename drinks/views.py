e4d7db1e95e3a9db296235ed8b6d96c9a8e6f70a
Maunducyrus <maunducyrus123@gmail.com>
2025-03-10 01:49:44 +0300

    views

--- drinks/views.py
+++ drinks/views.py
@@ -14,3 +14,14 @@
 #     serializer = DrinkSerializer(drinks, many=True)
 #     # return them as a JSON response
 #     return JsonResponse(serializer.data, safe=False)
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+def drink_list(request):
+    # Get all the drinks
+    drinks = Drink.objects.all()
+    # Serialize them
+    serializer = DrinkSerializer(drinks, many=True)
+    # Return them as a JSON response
+    return JsonResponse(serializer.data, safe=False)
