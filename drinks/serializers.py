edf2e6c7e313e292a5c704e3d738e79f19bb05e2
Maunducyrus <maunducyrus123@gmail.com>
2025-03-10 00:48:10 +0300

    class, and imports

--- drinks/serializers.py
+++ drinks/serializers.py
@@ -1,1 +1,6 @@
 # Describes process of going from python objects to JSON and vice versa.
+from rest_framework import serializers
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    
