64536df1c23fd20671b6522f5104d74e401058ba
Maunducyrus <maunducyrus123@gmail.com>
2025-03-10 12:41:49 +0300

    import response

--- drinks/views.py
+++ drinks/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Other api endpoint methods 
 
